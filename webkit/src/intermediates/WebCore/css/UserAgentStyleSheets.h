ad971607a5094ad30ebd00d9b92d4f83f37ffd7b
Pirama Arumuga Nainar <pirama@google.com>
2017-08-07 13:46:57 -0700

    Remove all files
    This project is now used for scripts to run platform projects as
    benchmarks for Clang.
    
    Test: None
    
    Change-Id: Icdd307b856fcbd887205dbd99fab5816d6a6c4c9

--- webkit/src/intermediates/WebCore/css/UserAgentStyleSheets.h
+++
@@ -1,6 +0,0 @@
-namespace WebCore {
-extern const char htmlUserAgentStyleSheet[8750];
-extern const char quirksUserAgentStyleSheet[359];
-extern const char sourceUserAgentStyleSheet[2010];
-extern const char mediaControlsUserAgentStyleSheet[2582];
-}
